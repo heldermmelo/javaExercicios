016c00acf847f396188acb5be9f09d1e1b7c8a90
e_helder <e_helder@172.18.77.198>
2016-01-26 11:44:25 -0300

    "Dado" funcional

--- src/Dado.java
+++ src/Dado.java
@@ -1,5 +1,6 @@
 import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,20 +9,27 @@ public class Dado {
 	public static void main(String[] args) {
 		int lados;
 		int quantos;
+		@SuppressWarnings("resource")
 		Scanner leitor = new Scanner(System.in);
 		System.out.print("Quantos lados vai querer no seu dado?:");
 		lados = leitor.nextInt();
 		System.out.print("Quantos dados vai querer lançar?:");
 		quantos = leitor.nextInt();
+		lancamento(lados,quantos);
 	}
 	
-	public ArrayList<Integer> lancamento(int nLados, int nDados){
+	public static ArrayList<Integer> lancamento(int nLados, int nDados){
 		ArrayList<Integer> resultados = new ArrayList<Integer>();
 		Random alea = new Random();
 		while (nDados>0){
 			
 			resultados.add(alea.nextInt(nLados)+1);
+			nDados--;
 		}
+		for(int i=0; i<resultados.size(); i++){
+			System.out.println(resultados.get(i).toString());
+		}
+		
 		return resultados;
 	}
 
